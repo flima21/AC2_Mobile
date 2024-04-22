32aadc3f257cfc0be4bb0d4c05304c343d5ef604
Felipe Pedroso de Lima <felipe.pedroso2108@gmail.com>
2024-04-22 19:32:19 -0300

    Criação de layout e classe Filme

---
+++ app/src/main/java/com/example/ac2/models/Filme.java
@@ -0,0 +1,60 @@
+package com.example.ac2.models;
+
+public class Filme {
+    private String titulo;
+    private String sinopse;
+    private String diretor;
+    private int ano;
+    private int imagems;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getImagems() {
+        return imagems;
+    }
+
+    public void setImagems(int imagems) {
+        this.imagems = imagems;
+    }
+
+    public Filme(String titulo, String sinopse, String diretor, int ano, int imagems) {
+        this.titulo = titulo;
+        this.sinopse = sinopse;
+        this.diretor = diretor;
+        this.ano = ano;
+        this.imagems = imagems;
+    }
+
+    public Filme() {
+    }
+}
